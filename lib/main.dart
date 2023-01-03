7cd774295ec85adb558afc0a4fd983cddec28c76
Ahmet Basmaji <96287253+Eng-Ahmet@users.noreply.github.com>
2023-01-03 11:49:40 +0300

    first commit

--- lib/main.dart
+++ lib/main.dart
@@ -24,7 +24,7 @@ void main() async {
 }
 
 class MyApp extends StatelessWidget {
-  MyApp({Key? key}) : super(key: key);
+  const MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
